ded987ccb656cc43915c7f289c7bedb4a3f399b3
aphrem-thomas <aphremthomas@gmail.com>
2019-02-25 12:24:56 +0530

    conflicts resolved

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Link} from 'react-router-dom';
 import { routes } from './../../routes/global.routes';
 import styles from './App.css';
 import RoutesGenerator from '../../components/RoutesGenerator/RoutesGenerator';
-=======
 import SayHello from './../../components/SayHello/SayHello';
 import Counter from './../../components/Counter/Counter';
 import { Button } from '@rzt/eva';
->>>>>>> 04a95acccbdcddf34349183a857654d39e5f824e
 
 class App extends Component {
   constructor(props, context) {
